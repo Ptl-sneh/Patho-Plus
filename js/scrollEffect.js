ba3747af8605c53996573f1d534755f947d8c561
Ptl-sneh <snehpatel641@gmail.com>
2025-02-13 19:36:29 +0530

    d

--- js/scrollEffect.js
+++ js/scrollEffect.js
@@ -3,7 +3,10 @@ const scrollContents = document.querySelectorAll('.scroll-content');
 function checkScroll() {
     scrollContents.forEach((content) => {
         const position = content.getBoundingClientRect().top;
+        // console.log(position);
+        
         const viewportHeight = window.innerHeight;
+        // console.log("height" + viewportHeight)
 
         if (position < viewportHeight - 100) {
             content.classList.add('in-view');
